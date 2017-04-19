03bcbe456294370d340a1785a0d86d93886c2428
Unknown <lukel9914@gmail.com>
2017-04-19 21:44:00 +0100

    ffiixx

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -1,4 +1,4 @@
-exports.run = function (client, msg, args, guilds) {
+exports.run = function (client, msg, args, guilds, db) {
 
 	if (!permissions.isAdmin(msg.member, msg.guild.id, db)) return msg.channel.createMessage({ embed: {
 		color: 0x1E90FF,
