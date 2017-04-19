b618bb6fcc769d581515d59556dcfb34275b7fb5
Unknown <lukel9914@gmail.com>
2017-04-19 21:37:20 +0100

    fix

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -1,6 +1,6 @@
 exports.run = function (client, msg, args, guilds, db) {
 
-	if (!permissions.isAdmin(msg.member, msg.guild.id)) return msg.channel.createMessage({ embed: {
+	if (!permissions.isAdmin(msg.member, msg.guild.id, db)) return msg.channel.createMessage({ embed: {
 		color: 0x1E90FF,
 		title: "Insufficient Permissions",
 	}});
