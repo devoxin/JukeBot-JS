f7bbefb59f9700351c3cd9713fa35300745737ac
Luke Livingstone <lukel9914@gmail.com>
2017-04-28 17:02:22 +0100

    fix unadmin2

--- src/commands/unadmin.js
+++ src/commands/unadmin.js
@@ -1,4 +1,4 @@
-exports.run = function (client, msg, args) {
+exports.run = function (client, msg, args, db) {
 
 	if (!permissions.isAdmin(msg.member, msg.channel.guild.id, db)) return msg.channel.createMessage({ embed: {
 		color: 0x1E90FF,
