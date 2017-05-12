789f9c1d0d43cd98286fe73b68de21bd44c3763c
Luke Livingstone <lukel9914@gmail.com>
2017-05-12 23:03:19 +0100

    Allow people to forceskip songs they've queued

--- src/commands/forceskip.js
+++ src/commands/forceskip.js
@@ -1,6 +1,6 @@
 exports.run = function (client, msg, args) {
 
-	if (!permissions.isAdmin(msg.member)) return msg.channel.createMessage({ embed: {
+	if (!permissions.isAdmin(msg.member) && guilds[msg.channel.guild.id].queue[0].req !== msg.author.id) return msg.channel.createMessage({ embed: {
 		color: 0x1E90FF,
 		title: "Insufficient Permissions",
 	}});
