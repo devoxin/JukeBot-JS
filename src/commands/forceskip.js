c579687671ff222088449fc17a40c9d54a823115
Luke Livingstone <lukel9914@gmail.com>
2017-05-12 23:07:17 +0100

    make forceskip great again

--- src/commands/forceskip.js
+++ src/commands/forceskip.js
@@ -1,13 +1,13 @@
 exports.run = function (client, msg, args) {
 
+	if (!client.voiceConnections.get(msg.channel.guild.id) || !guilds[msg.channel.guild.id].queue[0]) return msg.channel.createMessage({ embed: {
+		color: 0x1E90FF,
+		title: "There's no playback activity."
+	}});
+
 	if (!permissions.isAdmin(msg.member) && guilds[msg.channel.guild.id].queue[0].req !== msg.author.id) return msg.channel.createMessage({ embed: {
 		color: 0x1E90FF,
 		title: "Insufficient Permissions",
-	}});
-
-	if (!client.voiceConnections.get(msg.channel.guild.id)) return msg.channel.createMessage({ embed: {
-		color: 0x1E90FF,
-		title: "There's no playback activity."
 	}});
 
 	client.voiceConnections.get(msg.channel.guild.id).stopPlaying();
