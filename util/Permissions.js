f4d5753bbaa3831117f304929ebd822ecea430e0
Luke Livingstone <lukel9914@gmail.com>
2017-05-18 11:28:59 +0100

    Permissions Handler fix

--- util/Permissions.js
+++ util/Permissions.js
@@ -1,8 +1,9 @@
-const owners = config.prop.owners;
+const owners = config.owners;
 
 module.exports = {
 
 	hasRole(member, rolename) {
+		if (!member.guild) return false;
 		let role = member.guild.roles.find(r => r.name === rolename);
 		return (role && member.roles.includes(role.id));
 	},
@@ -12,14 +13,14 @@ module.exports = {
 	},
 
 	isBlocked(member) {
-		let hdb = require(`../src/config.json`).prop.blocked;
+		let hdb = require(`../src/config.json`).hardblock;
 		delete require.cache[require.resolve(`../src/config.json`)];
 
-		return ((module.exports.hasRole(member, "NoMusic") || hdb.includes(member)) && !owners.includes(member) && member.guild.ownerID !== member);
+		return ((module.exports.hasRole(member, "NoMusic") || hdb.includes(member.id)) && !owners.includes(member.id) && member.guild.ownerID !== member.id);
 	},
 
 	isDonator(member) {
-		let donators = require(`../src/config.json`).prop.donators;
+		let donators = require(`../src/config.json`).donators;
 		delete require.cache[require.resolve(`../src/config.json`)];
 
 		return (donators.includes(member) || owners.includes(member));
