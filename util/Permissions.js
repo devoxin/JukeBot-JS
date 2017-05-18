3bfe2c2e6a419e965bae17452421e504f529f243
Luke Livingstone <lukel9914@gmail.com>
2017-05-18 11:34:06 +0100

    lets try that again, shall we?

--- util/Permissions.js
+++ util/Permissions.js
@@ -1,4 +1,4 @@
-const owners = config.owners;
+const owners = config.prop.owners;
 
 module.exports = {
 
@@ -13,14 +13,14 @@ module.exports = {
 	},
 
 	isBlocked(member) {
-		let hdb = require(`../src/config.json`).hardblock;
+		let hdb = require(`../src/config.json`).prop.blocked;
 		delete require.cache[require.resolve(`../src/config.json`)];
 
 		return ((module.exports.hasRole(member, "NoMusic") || hdb.includes(member.id)) && !owners.includes(member.id) && member.guild.ownerID !== member.id);
 	},
 
 	isDonator(member) {
-		let donators = require(`../src/config.json`).donators;
+		let donators = require(`../src/config.json`).prop.donators;
 		delete require.cache[require.resolve(`../src/config.json`)];
 
 		return (donators.includes(member) || owners.includes(member));
