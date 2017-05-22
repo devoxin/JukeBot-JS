f9a983d3ac4151b919f49b004f45c6e7297bf83f
Luke Livingstone <lukel9914@gmail.com>
2017-05-22 19:21:59 +0100

    shuffle stuff

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -1,14 +1,22 @@
 exports.run = function (client, msg, args) {
 
-	if (!permissions.isAdmin(msg.member)) return msg.channel.createMessage({ embed: {
-		color: config.options.embedColour,
-		title: "Insufficient Permissions",
-	}});
+	if (!permissions.isAdmin(msg.member))
+		return msg.channel.createMessage({ embed: {
+			color: config.options.embedColour,
+			title: "Insufficient Permissions",
+		}});
 
-	if (!client.voiceConnections.get(msg.channel.guild.id)) return msg.channel.createMessage({ embed: {
-		color: config.options.embedColour,
-		title: "There's no playback activity."
-	}});
+	if (!client.voiceConnections.get(msg.channel.guild.id))
+		return msg.channel.createMessage({ embed: {
+			color: config.options.embedColour,
+			title: "There's no playback activity"
+		}});
+
+	if (guilds[msg.channel.guild.id].queue.length < 3)
+		return msg.channel.createMessage({ embed: {
+			color: config.options.embedColour,
+			title: "There's not enough songs in the queue to shuffle",
+		}});
 
 	let tempqueue = guilds[msg.channel.guild.id].queue.slice(1);
 	let curInd = tempqueue.length, tempVal, randInd;
