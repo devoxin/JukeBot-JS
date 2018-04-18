b702a07446367ae59c6ccb7d5a26481c61c4d3b1
Devoxin <lukel9914@gmail.com>
2018-04-18 23:16:33 +0100

    Paginate

--- src/commands/bash.js
+++ src/commands/bash.js
@@ -12,17 +12,58 @@ exports.run = async function(client, msg, args) {
     const m = await msg.channel.createMessage(`❯_ ${  args.join(' ')}`);
     bash.exec(`${args.join(' ')}`, async (e, stdout, stderr) => {
         if (e) return m.edit(`\`\`\`js\n${e.message}\n\`\`\``);
-        if (stdout.length > 2000 || stderr.length > 2000) {
-            console.log(stdout);
-            console.error(stderr);
-            m.edit(`❯_ ${args.join(' ')}\nOutput too big, check console.`);
-        } else {
-            if (!stdout && !stderr) return m.addReaction('\u2611');
-            stdout && msg.channel.createMessage(`**Info**\n${  stdout !== '' ? `\`\`\`js\n${  stdout  }\`\`\`` : 'No information.'}`);
-            stderr && msg.channel.createMessage(`**Errors**\n${  stderr !== '' ? `\`\`\`js\n${  stderr  }\`\`\`` : 'No errors.'}`);
+
+        if (!stdout && !stderr) {
+            return msg.channel.createMessage('Completed without result.');
+        }
+
+        if (stdout) {
+            const pages = paginate(stdout, 1950);
+            for (const page of pages) {
+                await msg.channel.createMessage(`\`\`\`\n${page}\`\`\``);
+            }
+        }
+
+        if (stderr) {
+            const pages = paginate(stderr, 1950);
+            for (const page of pages) {
+                await msg.channel.createMessage(`\`\`\`\n${page}\`\`\``);
+            }
         }
     });
 };
+
+function paginate(text, limit = 2000) {
+    const lines = text.trim().split('\n');
+    const pages = [];
+
+    let chunk = '';
+
+    for (const line of lines) {
+        if (chunk.length + line.length > limit && chunk.length > 0) {
+            pages.push(chunk);
+            chunk = '';
+        }
+
+        if (line.length > limit) {
+            const lineChunks = line.length / limit;
+
+            for (let i = 0; i < lineChunks; i++) {
+                const start = i * limit;
+                const end = start + limit;
+                pages.push(line.slice(start, end));
+            }
+        } else {
+            chunk += `${line}\n`;
+        }
+    }
+
+    if (chunk.length > 0) {
+        pages.push(chunk);
+    }
+
+    return pages;
+}
 
 exports.usage = {
     main: '{prefix}{command}',
