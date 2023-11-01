63ca73b1104c02a3e4076a48a62e08d6d40740d7
Devoxin <lukel9914@gmail.com>
2023-11-01 20:58:09 +0000

    inline

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -8,14 +8,14 @@ exports.run = async function ({ client, msg }) {
   msg.channel.createMessage({ embed: {
     color: client.config.options.embedColour,
     title: `JukeBot v${version}`,
-    description: 'Created by Kromatic#0420',
+    description: 'Created by devoxin',
     fields: [
-      { name: 'Uptime',		  value: timeParser.formatSeconds(process.uptime()),						inline: true },
-      { name: 'RAM Usage',	  value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,      inline: true },
-      { name: 'Library',		  value: 'Eris',															inline: true },
-      { name: 'Streams',        value: `► ${playing}, ❚❚ ${paused}`,                                      inline: true },
-      { name: 'Servers',		  value: client.guilds.size,												inline: true },
-      { name: 'Latency',		  value: `${msg.channel.guild.shard.latency}ms`,							inline: true }
+      { name: 'Uptime',		 value: timeParser.formatSeconds(process.uptime()),						        inline: true },
+      { name: 'RAM Usage', value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`, inline: true },
+      { name: 'Library',	 value: 'Eris',															                          inline: true },
+      { name: 'Streams',   value: `► ${playing}, ❚❚ ${paused}`,                                 inline: true },
+      { name: 'Servers',	 value: client.guilds.size.toString(), 					                     inline: true },
+      { name: 'Latency',	 value: `${msg.channel.guild.shard.latency}ms`,						          	inline: true }
     ]
   } });
 
